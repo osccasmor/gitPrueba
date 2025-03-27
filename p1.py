61185785c2f95658ee59a40181bb1732276f76ce
Mi nombre <Correo fidiana>
2025-03-27 11:20:05 +0100

    Otra linea fichero python 1 desde VSC rama master

--- p1.py
+++ p1.py
@@ -1,2 +1,3 @@
 print("Agrego nueva linea fichero python 1 desde VSC")
 print("Agrego nueva línea fichero python 1 desde VSC rama master")
+print("Otra línea fichero python 1 desde VSC rama master")
